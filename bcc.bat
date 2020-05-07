82dbede075885ce38dcf1d1e15fd2ac557550438
Stephane Champailler <schampailler@skynet.be>
2020-05-07 21:20:03 +0200

    improvement

--- bcc.bat
+++ bcc.bat
@@ -1,3 +1,4 @@
+@echo off
 python precalc.py
 
 \PORT-STC\opt\cc65\bin\ca65 -o td.o td.s
@@ -9,7 +10,9 @@ copy data\BLANK_PRODOS2.DSK NEW.DSK
 
 java -jar ..\bad_apple\AppleCommander-1.3.5.13-ac.jar -p NEW.DSK START  BIN 0x6000  < THREED
 
+dir THREED
 
 REM -speed 40
 REM c:\port-stc\opt\mame\mame64 apple2p -skip_gameinfo -window -nomax -flop1 BAD_APPLE.DSK -flop2 BAD_APPLE_DATA.DSK -rp bios
+
 \PORT-STC\opt\applewin\Applewin.exe -d1 NEW.DSK
