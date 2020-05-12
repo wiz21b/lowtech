9abc91df0ebdb12af1f60f5d1083ef73f0f9912f
Stéphane Champailler <schampailler@skynet.bewq>
2020-05-12 22:52:45 +0200

    better printing of source code

--- bcc.sh
+++ bcc.sh
@@ -15,6 +15,9 @@ cp data/BLANK_PRODOS2.DSK NEW.DSK
 java -jar ../bad_apple/AppleCommander-1.3.5.13-ac.jar -p NEW.DSK START    BIN 0x6000 < THREED
 java -jar ../bad_apple/AppleCommander-1.3.5.13-ac.jar -p NEW.DSK PRORWTS  BIN 0x0800 < prorwts2#060800
 
+# Generate listing (you can print in firefox, 2 pages side by side)
+source-highlight --src-lang asm -f html --doc -c=asm-style.css  --lang-def asm.lang vline.s
+
 
 # \PORT-STC\opt\applewin\Applewin.exe -d1 NEW.DSK
 mame apple2e -skip_gameinfo -window -nomax -flop1 NEW.DSK -rp ../bad_apple/bios -sound none -speed 1
